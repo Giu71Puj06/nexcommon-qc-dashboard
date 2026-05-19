9f2fd59752189ebf19728a38686471b108648efb
Giu71Puj06 <g.pizzi@itscontrollitecnici.it>
2026-05-19 11:11:55 +0200

    Update read-bcf.ts

--- lib/dashboard-pm/read-bcf.ts
+++ lib/dashboard-pm/read-bcf.ts
@@ -1,1 +1,20 @@
+import JSZip from "jszip";
 
+export async function readBCF(file: File) {
+  const zip = await JSZip.loadAsync(file);
+
+  const issues: any[] = [];
+
+  for (const fileName of Object.keys(zip.files)) {
+    if (fileName.endsWith("markup.bcf")) {
+      const content = await zip.files[fileName].async("text");
+
+      issues.push({
+        file: fileName,
+        raw: content,
+      });
+    }
+  }
+
+  return issues;
+}
